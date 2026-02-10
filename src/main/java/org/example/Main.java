92e6d0af13c844df52e1ae5b0cea56d4169042db
JYacuma <jyacuma@ucundinamarca.edu.co>
2026-02-10 12:35:58 -0500

    segundo cambio

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,8 @@ package org.example;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-
+    @Override
+    public String toString() {
+        return super.toString();
+    }
 }
